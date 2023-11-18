219a10b18b463b8c73b76d9ead850d1bcdace7ad
John Lucas <joohnzay@gmail.com>
2023-11-18 18:03:51 -0300

    (start)

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [busLocation, setBusLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão para acesso à localização foi negada.');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setBusLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+      });
+    })();
+  }, []);
+
+  if (!busLocation) {
+    return (
+      <View style={styles.container}>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: busLocation.latitude,
+          longitude: busLocation.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* Marcador do ônibus */}
+        <Marker coordinate={{ latitude: busLocation.latitude, longitude: busLocation.longitude }}>
+          <View style={styles.userMarker} />
+        </Marker>
+      </MapView>
+      {errorMsg && <Text>{errorMsg}</Text>}
     </View>
   );
 }
@@ -13,8 +55,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  userMarker: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'blue', // Altera a cor do marcador para azul
+    borderWidth: 2,
+    borderColor: 'white',
   },
 });
