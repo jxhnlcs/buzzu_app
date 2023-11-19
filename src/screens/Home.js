b8a03cfd78c5cf2c8d2cc4dc404af890a6f085bb
unknown <mauriciolobo509@gmail.com>
2023-11-18 23:20:24 -0300

    Container Para onde vamos , 99%

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,6 +7,10 @@ import * as Location from 'expo-location';
 export default function App() {
   const [busLocation, setBusLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [lastDestinations, setLastDestinations] = useState([
+    'Shopping Boulevard',
+    'Parque da Cidade',
+  ]);
 
   useEffect(() => {
     (async () => {
@@ -54,6 +58,15 @@ export default function App() {
             <AntDesign name="search1" size={24} color="white" style={styles.searchIcon} />
             <Text style={styles.buttonText}>Para onde vamos?</Text>
           </TouchableOpacity>
+          <View style={styles.historyContainer}>
+            {lastDestinations.map((destination, index) => (
+              <View key={index} style={styles.destinationContainer}>
+                <AntDesign name="clockcircleo" size={24} color="gray" style={styles.destinationIcon} />
+                <Text style={styles.lastDestinationText}>{destination}</Text>
+              </View>
+            ))}
+            <View style={styles.separator}></View>
+          </View>
         </View>
       </View>
 
@@ -93,16 +106,16 @@ const styles = StyleSheet.create({
     height: 185,
     padding: 16,
     borderRadius: 20,
-    borderWidth: 2,
-    borderColor: 'gray',
+    borderWidth: 1,
+    borderColor: 'rgba(128, 128, 128, 0.5)',
   },
   button: {
     backgroundColor: '#076DDB',
     borderColor: '#3B82F6',
     borderWidth: 2,
     borderRadius: 8,
-    width: 329, // largura desejada
-    height: 49, // altura desejada
+    width: 329,
+    height: 49,
     padding: 8,
     flexDirection: 'row',
     alignItems: 'center',
@@ -114,5 +127,26 @@ const styles = StyleSheet.create({
   },
   searchIcon: {
     marginRight: 10,
+  },
+  historyContainer: {
+    marginTop: 10,
+  },
+  destinationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  destinationIcon: {
+    marginRight: 10,
+  },
+  lastDestinationText: {
+    fontSize: 16,
+    color: 'gray',
+  },
+  separator: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+    opacity: 0.2,
+    top:-55,
   },
 });
